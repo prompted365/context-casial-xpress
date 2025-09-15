342f5b26e731f4001c31c131bf6acec466c44699
Breyden Taylor <124335295+prompted365@users.noreply.github.com>
2025-09-14 22:06:05 -0400

    Fix licensing: casial-core stays MIT/Apache-2.0, casial-server and casial-wasm use Fair Use license v0.1.3

---
+++ target/debug/deps/hyper_rustls-5fdebe81c52513e8.d
@@ -0,0 +1,9 @@
+/Users/breydentaylor/context-casial-xpress/target/debug/deps/hyper_rustls-5fdebe81c52513e8.d: /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/lib.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/config.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector/builder.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/stream.rs
+
+/Users/breydentaylor/context-casial-xpress/target/debug/deps/libhyper_rustls-5fdebe81c52513e8.rmeta: /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/lib.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/config.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector/builder.rs /Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/stream.rs
+
+/Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/lib.rs:
+/Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/config.rs:
+/Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector.rs:
+/Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/connector/builder.rs:
+/Users/breydentaylor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hyper-rustls-0.27.7/src/stream.rs:
